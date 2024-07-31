d7f579a5b512a668f08b54e85ddadceb9dbea79e
Phanusonboss <phanusonbosslove@gmail.com>
2024-07-31 13:32:02 +0700

    modified surname variable and set name and surname text default

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -6,13 +6,23 @@
       @reset="onReset"
       class="q-gutter-md"
     >
+    <!-- Name -->
       <q-input
         filled
         v-model="name"
         label="Your name *"
-        hint="Name and surname"
+        hint="Name"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'Please type your name']"
+      />
+    <!-- surname -->
+      <q-input
+        filled
+        v-model="name"
+        label="Your surname *"
+        hint="surname"
+        lazy-rules
+        :rules="[ val => val && val.length > 0 || 'Please type your surname']"
       />
 
       <q-input
@@ -46,12 +56,14 @@ export default {
   setup () {
     const $q = useQuasar()
 
-    const name = ref(null)
+    const name = ref("ภานุสรณ์")
+    const surname = ref("คำโคกกรวด")
     const age = ref(null)
     const accept = ref(false)
 
     return {
       name,
+      surname,
       age,
       accept,
 
